84981d43e49e00b7cd09eccc4d1678be3622cacf
Riza Semih Koca <rsemihkoca@outlook.com>
2023-08-13 15:04:56 +0300

    commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, field_validator
+from pymongo import MongoClient
+import os
+
+# Constants
+class Constants:
+    Floor00 = "Floor00"
+    Floor01 = "Floor01"
+    Floor02 = "Floor02"
+    Floor03 = "Floor03"
+    ALL_FLOORS = [Floor00, Floor01, Floor02, Floor03]
+
+# FastAPI app initialization
+app = FastAPI()
+
+# MongoDB connection setup
+CONNECTION_STRING = os.environ.get("CONNECTION_STRING", "mongodb://localhost:27017")
+client = MongoClient(CONNECTION_STRING)
+db = client.MIK_Database
+collection = db.MIK_Collection
+
+# Models
+class FloorQuery(BaseModel):
+    floor: str
+
+    @field_validator("floor")
+    def valid_floor(cls, value):
+        if value not in Constants.ALL_FLOORS:
+            raise ValueError(f"Floor should be one of {', '.join(Constants.ALL_FLOORS)}")
+        return value
+
+@app.get("/")
+async def health_check():
+    return {"status": "ok"}
+
+@app.post("/get_floor/")
+async def get_floor(payload: FloorQuery):
+    cursor = collection.find({"floor": payload.floor})
+    floor_data = list(cursor)
+    for doc in floor_data:
+        doc["_id"] = str(doc["_id"])  # Convert ObjectIDs to strings for JSON serialization
+    return {"data": floor_data}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
